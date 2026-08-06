90afb8c86990c824953226da3cc08c7dc5a43eb7
Dereje <deregood29@gmail.com>
2026-08-06 12:03:42 +0300

    ctms

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
             />
           </div>
           <span className="text-xl font-bold tracking-tight text-white">
-            Construct<span className="text-amber-500">Co</span>
+            C<span className="text-amber-500">-TMS</span>
           </span>
         </Link>
 
